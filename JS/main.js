534474f0688f6e740e82207f0591a7e7aa97f9f9
Hudson <gdog2u@gmail.com>
2018-12-20 01:16:32 -0500

    Add functioning weather display
    Added functionality to the weather section of the page. Weather is now
    updated on page load. Weather is pulled using the OpenWeatherMaps API.
    Updated CSS to help with this section.

--- JS/main.js
+++ JS/main.js
@@ -1,2 +1,45 @@
 // city id for Dayton, OH via https://OpenWeatherMap.org
 var weatherCityID = 4919553;
+var weatherAPI = "d7235563258d3bbb6ba494080319486e";
+var stockAPI = "aT12THC6tWcnMvBiI6tSSBrCPQpcwKuSn3cDKjQsunJM1AQPNnJRc9af0e7v";
+
+function getCurrentWeather(){
+    $.ajax({
+        url: "https://api.openweathermap.org/data/2.5/weather",
+        method: "get",
+        data: {
+            id: weatherCityID,
+            APPID: weatherAPI,
+            units: "imperial"
+        },
+        success: function(data){
+            console.log(data);
+            updateWeatherDisplay(data);
+        },
+        error: function(data){
+            let response = JSON.parse(data.responseText);
+            console.log(response.message);
+        }
+    });
+}
+
+function updateWeatherDisplay(weatherData){
+    let round = Math.round;
+    // Update current temp
+    $('#currentTemp').text(round(weatherData.main.temp));
+    // Update current low
+    $('#currentLow').text(round(weatherData.main.temp_min));
+    // Update current high
+    $('#currentHigh').text(round(weatherData.main.temp_max));
+
+    // Find current weather image
+    let image = "";
+    switch(weatherData.weather[0].main){
+        case "Clouds":
+            image = "partial-cloudy-night";
+            break;
+    }
+
+    // Update current weatehr image
+    $('#currentIcon').attr("src", "images/"+image+".png");
+}
