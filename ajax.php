63b126fe2a49d7df31814676a38832d0b2359276
ghudson <ghudson@dorothylane.com>
2018-12-31 15:15:51 -0500

    Add markTaskAs function and ajax receivers
    Added end points for ajax calls to mark tasks as completed or
    deleted. Added the function markTaskAs to handle both of them.
    Function also marks a task's children the same.
    Fixed ordering of getTasks and limited to top 8 results.

--- ajax.php
+++ ajax.php
@@ -1,5 +1,5 @@
 <?php
-
+    // ini_set()
     if($_SERVER["REQUEST_METHOD"] == "POST"){
         header('Content-Type: application/json');
         /*
@@ -7,40 +7,7 @@
         */
         $response = array(
             "responseCode" => 200,
-            "responseData" => "" //array( // Test data
-                // "tasks" => array(
-                //     array(
-                //         "id" => 1,
-                //         "parent" => -1,
-                //         "task" => "Buy mushrooms"
-                //     ),
-                //     array(
-                //     	"id" => 2,
-                //         "parent" => -1,
-                //         "task" => "Give David his gift"
-                //     ),
-                //     array(
-                //         "id" => 3,
-                //         "parent" => -1,
-                //         "task" => "2019 IT Tasks"
-                //     ),
-                //     array(
-                //         "id" => 4,
-                //         "parent" => 3,
-                //         "task" => "Ask about Better Health Club"
-                //     ),
-                //     array(
-                //         "id" => 5,
-                //         "parent" => 3,
-                //         "task" => "Ask about associate tests"
-                //     ),
-                //     array(
-                //         "id" => 7,
-                //         "parent" => -1,
-                //         "task" => "Buy mum a present"
-                //     )
-                // )
-            // )
+            "responseData" => ""
         );
 
         $func = $_POST["func"];
@@ -62,6 +29,20 @@
                 $hashArray = hashArray($response["responseData"]);
                 $response["responseData"]["hash"] = $hashArray;
             }
+
+            echo json_encode($response);
+        }
+        elseif($func == "completeTask"){
+            $result = markTaskAs($_POST["taskID"], 1);
+            $response["responseCode"] = $result["code"];
+            $response["responseData"] = $result["message"];
+
+            echo json_encode($response);
+        }
+        elseif($func == "deleteTask"){
+            $result = markTaskAs($_POST["taskID"], 2);
+            $response["responseCode"] = $result["code"];
+            $response["responseData"] = $result["message"];
 
             echo json_encode($response);
         }
@@ -90,7 +71,8 @@
                 FROM Tasks AS Task
                     LEFT JOIN Tasks AS Parent ON Parent.TaskID = Task.ParentID
                 WHERE Task.Deleted = 0 AND (Task.Completed=0 AND IFNULL(Parent.Completed, 0) = 0)
-                ORDER BY DueDate ASC;
+                ORDER BY IFNULL(Task.DueDate, '2099-01-01 00:00:00') ASC
+                LIMIT 8;
             ");
 
             while($task = $result->fetch(PDO::FETCH_ASSOC)){
@@ -101,5 +83,64 @@
         }
 
         return $return;
+    }
+
+    /**
+     * Mark a task task either Completed or deleted
+     * Also recursively marks a task's children as the same status
+     */
+    function markTaskAs($taskID, $markAs, $recursed=0){
+        // Param checking
+        if(preg_match("/^\d+/", $taskID) != 1 ){
+            return array("code"=>-1,"message"=>"\$taskID must be an integer");
+        }
+        if(preg_match("/[12]/", $markAs) != 1){
+            return array("code"=>-1,"message"=>"\$markAs must be 1 if completed or 2 if deleted");
+        }
+
+        // DB Connection
+        $conn = new PDO("mysql:host=localhost;dbname=MyHome", "root", "Henri2u2");
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        // Set the string for which field we're changing
+        $field = "";
+        if($markAs == 1){
+            $field = "Completed";
+        }else{
+            $field = "Deleted";
+        }
+
+        try{
+            // Prepare update statement
+            $update = $conn->prepare("
+                UPDATE tasks
+                SET $field=1
+                WHERE TaskID=:taskID
+            ");
+            $update->bindParam(":taskID", $taskID);
+
+            $update->execute();
+
+            // Find any tasks that have $taskID as their parent
+            $findChildren = $conn->prepare("
+                SELECT TaskID
+                FROM Tasks
+                WHERE ParentID=:parentID
+            ");
+            $findChildren->bindParam(":parentID", $taskID);
+
+            $findChildren->execute();
+
+            // Loop over children, marking them the same as the initial call
+            while($task = $findChildren->fetch(PDO::FETCH_ASSOC)){
+                markTaskAs($task["TaskID"], $markAs, 1);
+            }
+        }catch(PDOException $e){
+            return array("code"=>-2, "message"=>$e->getMessage());
+        }
+
+        if(!$recursed){
+            return array("code"=>1,"message"=>"success");
+        }
     }
 ?>
